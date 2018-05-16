c49829c65449a2d04c50acf13e9eabc309c37969
jchaelgonzales <jchaelgonzales@gmail.com>
2018-05-16 10:56:17 +0800

    First Commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,18 +2,18 @@ class User < ApplicationRecord
 	has_many :request
 	has_secure_token :auth_token
 	has_secure_password
-	validates_uniqueness_of :username
+	validates_uniqueness_of :username, :email
 
-	# validates :username, presence: true
-	# validates :email, presence: true
-	# validates :password, presence: true
-	# validates :first_name, presence: true
-	# validates :last_name, presence: true
-	# validates :birthday, presence: true
-	# validates :gender, presence: true
-	# validates :blood_type, presence: true
-	# validates :address_street, presence: true
-	# validates :address_city, presence: true
-	# validates :address_province, presence: true
-	# validates :contact_info, presence: true
+	validates :username, presence: true
+	validates :email, presence: true
+	validates :password, presence: true, :on => :create
+	validates :first_name, presence: true
+	validates :last_name, presence: true
+	validates :birthday, presence: true
+	validates :gender, presence: true
+	validates :blood_type, presence: true
+	validates :address_street, presence: true
+	validates :address_city, presence: true
+	validates :address_province, presence: true
+	validates :contact_info, presence: true
 end
