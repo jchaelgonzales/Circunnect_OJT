942987ccb7173aa064c315939b29dab5a11873ae
jchaelgonzales <jchaelgonzales@gmail.com>
2018-05-16 15:27:33 +0800

    Added new columns

--- db/migrate/20180514174319_create_requests.rb
+++ db/migrate/20180514174319_create_requests.rb
@@ -2,6 +2,8 @@ class CreateRequests < ActiveRecord::Migration[5.2]
   def change
     create_table :requests do |t|
       t.string :request_accepted
+      t.string :request_finder
+      t.string :request_donor
 
       t.timestamps
     end
