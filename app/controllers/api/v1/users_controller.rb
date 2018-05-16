99f98c2f8ce4821648c7e1bb71233cc6751a91f5
jchaelgonzales <jchaelgonzales@gmail.com>
2018-05-16 10:55:24 +0800

    First Commit

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -19,6 +19,6 @@ class Api::V1::UsersController < ApplicationController
 	private
 
 	def users_params
-		params.permit(:username, :email, :password, :password_confirmation, :first_name, :last_name, :birthday, :gender, :blood_type, :address_street, :address_city, :address_province, :contact_info)
+		params.permit(:username, :email, :password, :password_confirmation, :first_name, :last_name, :birthday, :gender, :blood_type, :address_street, :address_city, :address_province, :contact_info, :user_type)
 	end
 end
