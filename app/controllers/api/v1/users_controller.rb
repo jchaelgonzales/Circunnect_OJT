bed1cdf8e2296d2d6007cf452a13d3cda70a0373
jchaelgonzales <jchaelgonzales@gmail.com>
2018-05-17 20:42:57 +0800

    Added update method

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,24 +1,33 @@
 class Api::V1::UsersController < ApplicationController
-	# skip_before_action :verify_authenticity_token
 
 	def index
 		users = User.order('created_at DESC');
-		render json: {status: 'SUCCESS', message:'Loaded users', data:users}, status: :ok
+		render json: users.reload, status: :ok
 	end
 
 	def create
 		user = User.new(users_params)
 
 		if user.save
-			render json: {status: 'SUCCESS', message:'Saved user', data:user}, status: :ok
+			render json: user.reload, status: :ok
 		else
 			render json: user.errors, status: :bad_request
 		end
 	end
 
+	def update
+		user = User.find(params[:id])
+
+		if user.update_attributes(users_params)
+			render json: user.reload, status: :ok
+		else
+			render json: user.errors, status: :bad_request
+		end
+    end
+
 	private
 
 	def users_params
-		params.permit(:username, :email, :password, :password_confirmation, :first_name, :last_name, :birthday, :gender, :blood_type, :address_street, :address_city, :address_province, :contact_info, :user_type)
+		params.permit(:username, :email, :password, :password_confirmation, :first_name, :last_name, :birthday, :gender, :blood_type, :address_street, :address_city, :address_province, :contact_info, :avatar, :user_type)
 	end
 end
