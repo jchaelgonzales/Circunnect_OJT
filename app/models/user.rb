e7eb407652c7c876d2f137053b24e058892b8632
jchaelgonzales <jchaelgonzales@gmail.com>
2018-05-22 09:32:39 +0800

    Added user validation & invalidation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,17 +5,29 @@ class User < ApplicationRecord
 	has_secure_password
 	validates_uniqueness_of :username, :email
 
-	validates :username, presence: true
-	validates :email, presence: true
-	validates :password, presence: true, :on => :create
-	validates :first_name, presence: true
-	validates :last_name, presence: true
-	validates :birthday, presence: true
-	validates :gender, presence: true
-	validates :blood_type, presence: true
-	validates :address_street, presence: true
-	validates :address_city, presence: true
-	validates :address_province, presence: true
-	validates :contact_info, presence: true
+	def invalidate_token
+    	self.update_columns(auth_token: nil)
+ 	end
+
+  	def self.valid_login?(username, password)
+    	user = find_by(username: username)
+
+    	if user && user.authenticate(password)
+      		user
+    	end
+  	end
+
+	# validates :username, presence: true
+	# validates :email, presence: true
+	# validates :password, presence: true, :on => :create
+	# validates :first_name, presence: true
+	# validates :last_name, presence: true
+	# validates :birthday, presence: true
+	# validates :gender, presence: true
+	# validates :blood_type, presence: true
+	# validates :address_street, presence: true
+	# validates :address_city, presence: true
+	# validates :address_province, presence: true
+	# validates :contact_info, presence: true
 	
 end
