8add0b9b8ba02f14f71051c5943816f4cdc4b111
jchaelgonzales <jchaelgonzales@gmail.com>
2018-05-22 09:28:55 +0800

    Added login & logout method

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,22 +1,44 @@
 class Api::V1::SessionsController < ApplicationController
-	def current_user
-    	@current_user ||= authentication_auth_token
-  	end
+  skip_before_action :require_login, only: [:authenticate_user], raise: false
 
-	def create
-		user = User.find_by_username(params[:username])
+  def login_user
+    if user = User.valid_login?(params[:username], params[:password])
+      	user.password_confirmation = params[:password]
+      	user.password = params[:password]
+    		user.regenerate_auth_token
+    		render json: { status: 'Success', data:user }, status: :ok
+  	else
+        render_unauthorized("Error with your login or password")
+    end
+  end
 
-		if user && user.authenticate(params[:password])
-			user.regenerate_auth_token
-			# render json: token, status: :ok
-
-			render json: { status: 'Success', data:user }, status: :ok
-		else
-			render json: { status: 'Wrong email or password' }, status: :unauthorized
-		end
-	end
-
-	def destroy
-    	current_user = nil
- 	end
+  def logout_user
+  	p current_user
+  	current_user.invalidate_token
+  	head :ok
+  end
 end
+
+
+
+# 	def current_user
+#     	@current_user ||= authentication_auth_token
+#   	end
+
+# 	def create
+# 		user = User.find_by_username(params[:username])
+
+# 		if user && user.authenticate(params[:password])
+# 			user.regenerate_auth_token
+# 			# render json: token, status: :ok
+
+# 			render json: { status: 'Success', data:user }, status: :ok
+# 		else
+# 			render json: { status: 'Wrong email or password' }, status: :unauthorized
+# 		end
+# 	end
+
+# 	def destroy
+#     	current_user = nil
+#  	end
+# end
