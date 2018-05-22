4615c8d65ce9a013f695e50d56a390ebea04a549
jchaelgonzales <jchaelgonzales@gmail.com>
2018-05-22 09:34:39 +0800

    Added foreign key

--- db/migrate/20180514174319_create_requests.rb
+++ db/migrate/20180514174319_create_requests.rb
@@ -1,9 +1,8 @@
 class CreateRequests < ActiveRecord::Migration[5.2]
   def change
     create_table :requests do |t|
-      t.string :request_accepted
-      t.string :request_finder
-      t.string :request_donor
+      t.boolean :request_accepted
+      t.references :user, foreign_key: true
 
       t.timestamps
     end
