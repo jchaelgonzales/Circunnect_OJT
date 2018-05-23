fca12473675a1884c763995bee3eb4cfbbe9b432
jchaelgonzales <jchaelgonzales@gmail.com>
2018-05-23 15:29:45 +0800

    Added new fields

--- db/migrate/20180514174319_create_requests.rb
+++ db/migrate/20180514174319_create_requests.rb
@@ -1,8 +1,10 @@
 class CreateRequests < ActiveRecord::Migration[5.2]
   def change
     create_table :requests do |t|
-      t.boolean :request_accepted
-      t.references :user, foreign_key: true
+      t.boolean :request_status
+      t.string :donor_username
+      t.string :request_username
+      
 
       t.timestamps
     end
